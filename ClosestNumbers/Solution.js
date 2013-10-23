8b103653b5cd52cda7c8d0a27119747d1b9dfb66
Trevor Senior <trevor@urban4m.com>
2013-10-23 11:45:41 -0400

    tabs -> spaces

--- ClosestNumbers/Solution.js
+++ ClosestNumbers/Solution.js
@@ -1,22 +1,22 @@
 function processData(input) {
-	var minDiff, diffPairs = {};
+    var minDiff, diffPairs = {};
 
     input.split('\n')[1].trim().split(' ') // pull out the numbers
     .map(function(number) { return parseInt(number, 10); }) // parse integers
     .sort(function(a, b) { return a - b; }) // sort ascending
     .forEach(function(number, i, arr) {
-    	var nextNumber = arr[i + 1];
-    	if (!nextNumber) return;
-    	var diff = Math.abs(number - nextNumber);
+        var nextNumber = arr[i + 1];
+        if (!nextNumber) return;
+        var diff = Math.abs(number - nextNumber);
 
-    	if (typeof minDiff === 'undefined' || diff < minDiff) minDiff = diff;
-    	if (typeof diffPairs[diff] === 'undefined') diffPairs[diff] = [];
+        if (typeof minDiff === 'undefined' || diff < minDiff) minDiff = diff;
+        if (typeof diffPairs[diff] === 'undefined') diffPairs[diff] = [];
 
-    	diffPairs[diff].push([number, nextNumber]);
+        diffPairs[diff].push([number, nextNumber]);
     });
 
     console.log(diffPairs[minDiff].reduce(function(prev, curr) {
-    	return prev.concat(curr);
+        return prev.concat(curr);
     }).join(' '));
 }
 
