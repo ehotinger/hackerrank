9264cd064147b3fd07aee0609f304c6b1544efde
Ankur Singh <ankursi@iitk.ac.in>
2017-02-20 18:42:47 +0530

    Update Solution.java

--- Candies/Solution.java
+++ Candies/Solution.java
@@ -4,55 +4,56 @@ import java.util.*;
 public class Solution {
 
     public static void main(String[] args) {
-        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        
         Scanner in=new Scanner (System.in);
         int N,i;
-        N=in.nextInt();
-        long S;S=0;        
-        int ar[]=new int[N];
+        N=in.nextInt();//total students
+        long S=0; //holding the total number of candies        
+        int students[]=new int[N];
         for(i=0;i<N;i++)
-            {ar[i]=in.nextInt();}
+            {students[i]=in.nextInt();}
         
-        int a[]=new int[N];
+        int candies[]=new int[N];
         
-        if(ar[0]>ar[1])
+        if(students[0]>students[1])
             {
-            a[0]=2;
-            a[1]=1;
+            candies[0]=2;
+            candies[1]=1;
             }
-        else if(ar[0]<ar[1])
+        else if(students[0]<students[1])
             {
-            a[0]=1;
-            a[1]=2;
+            candies[0]=1;
+            candies[1]=2;
             }
         else
-            {a[0]=1;
-            a[1]=1;
+            {
+            candies[0]=1;
+            candies[1]=1;
             }
          
         
-        for(i=1;i<(N-1);i++)
+        for(i=1;i<(N-1);i++)//going right incrementing the number of candies by one each time until i find a ranking crest
            {
-            if(ar[i]<ar[i+1])
+            if(students[i]<students[i+1])
                {
-               a[i+1]=a[i]+1;
+               candies[i+1]=candies[i]+1;
                }
             else
             {
-              a[i+1]=1;
+              candies[i+1]=1;
             }
             
            }
-        for(int z=(N-1);z>0;z--)
+        for(int z=(N-1);z>0;z--)//repeating the above step but to the left.
             {
-             if(ar[z]<ar[z-1])
-                 {if(a[z]>=a[z-1])
-                 {a[z-1]=a[z]+1;}
+             if(students[z]<students[z-1])
+                 {if(candies[z]>=candies[z-1])
+                 {candies[z-1]=candies[z]+1;}
                  }
             }
         for(int m=0;m<N;m++)
             {
-            S=S+a[m];
+            S=S+candies[m];
             }
         System.out.println(S);
         
